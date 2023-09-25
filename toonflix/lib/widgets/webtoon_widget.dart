aff397de2972147541d3947e4bedc816b14edb09
KimKW1007 <face676934@gmail.com>
2023-09-25 15:57:47 +0900

    feat : webtoon_widget - GestureDetector 추가

--- toonflix/lib/widgets/webtoon_widget.dart
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
@@ -8,38 +9,68 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(.5),
-                )
-              ]),
-          width: 250,
-          child: Image.network(
-            thumb,
-            headers: const {
-              "User-Agent":
-                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              var begin = const Offset(0, 1);
+              var end = Offset.zero;
+              var curve = Curves.ease;
+              var tween = Tween(
+                begin: begin,
+                end: end,
+              ).chain(
+                CurveTween(
+                  curve: curve,
+                ),
+              );
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
             },
+            pageBuilder: (context, anmation, secondaryAnimation) =>
+                DetailScreen(id: id, title: title, thumb: thumb),
+            fullscreenDialog: true,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(.5),
+                  )
+                ]),
+            width: 250,
+            child: Image.network(
+              thumb,
+              headers: const {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+              },
+            ),
           ),
-        ),
-      ],
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
